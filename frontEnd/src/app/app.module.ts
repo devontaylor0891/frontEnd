ab4a84b6d3b35fa7407efefbee92becfc920a659
Devon <devontaylor0891@gmail.com>
2017-07-29 16:20:34 -0600

    Working with material bootstrap

---
+++ frontEnd/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    MDBBootstrapModule.forRoot()
+  ],
+  providers: [],
+  schemas: [NO_ERRORS_SCHEMA],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
