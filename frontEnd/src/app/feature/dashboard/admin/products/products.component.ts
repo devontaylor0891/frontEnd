a6d75dba3d51cea408c3f775440073f91c7f6153
DEVON TAYLOR <devontaylor0891@gmail.com>
2017-08-15 00:59:02 +0000

    Updates to styles, move product data into component

--- frontEnd/src/app/feature/dashboard/admin/products/products.component.ts
+++ frontEnd/src/app/feature/dashboard/admin/products/products.component.ts
@@ -6,8 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent implements OnInit {
+  
+  products: [{productName: string, pricePerUnit: number, units: string, unitsPer: number, totalPrice: number, quantityAvailable: number, quantityPending: number, producerName: string}];
 
-  constructor() { }
+  constructor() {
+    
+    this.products = [
+      {
+        productName: 'Carrots',
+        pricePerUnit: 4,
+        units: 'lb',
+        unitsPer: 1,
+        totalPrice: 4,
+        quantityAvailable: 15,
+        quantityPending: 3,
+        producerName: 'Bob\'s Garden'
+      }, {
+        productName: 'Beans',
+        pricePerUnit: 4,
+        units: 'lb',
+        unitsPer: 1,
+        totalPrice: 3,
+        quantityAvailable: 22,
+        quantityPending: 4,
+        producerName: 'Frank\'s Garden'
+      }  
+    ]
+    
+  }
 
   ngOnInit() {
   }
