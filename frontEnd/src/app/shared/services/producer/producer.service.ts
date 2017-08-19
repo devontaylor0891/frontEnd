59593412d094bf985eb10df5abad64fcab6bab33
DEVON TAYLOR <devontaylor0891@gmail.com>
2017-08-19 16:32:10 +0000

    Commit small change to producer.service file

--- frontEnd/src/app/shared/services/producer/producer.service.ts
+++ frontEnd/src/app/shared/services/producer/producer.service.ts
@@ -10,35 +10,7 @@ export class ProducerService {
   
   url = '../../../../assets/api/producersAdmin.json';
     
-    //this will be where a getAll() method is called, to populate this array
-    producersAdmin: ProducerAdmin[] = [
-        // new ProducerAdmin(
-        //   4,
-        //   'Garden Farms',
-        //   'Moosomin',
-        //   'SK',
-        //   'This is a description',
-        //   'email@email.com',
-        //   'beet.png',
-        //   444,
-        //   555,
-        //   'Devon',
-        //   'Jan 1 2017'
-        // ),
-        // new ProducerAdmin(
-        //   4,
-        //   'Garden Farms',
-        //   'Moosomin',
-        //   'SK',
-        //   'This is a description',
-        //   'email@email.com',
-        //   'beet.png',
-        //   444,
-        //   555,
-        //   'Devon',
-        //   'Jan 1 2017'
-        // )
-    ];
+    producersAdmin: ProducerAdmin[] = [];
 
   constructor(private http: Http) { }
   
