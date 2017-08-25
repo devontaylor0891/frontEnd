e166063f58b1dba1f246e2e135dbf6037f0025c7
Devon <devontaylor0891@gmail.com>
2017-08-25 13:22:31 -0600

    Complete dash admin orders views/mock data

--- frontEnd/src/app/feature/dashboard/admin/orders/orders.component.ts
+++ frontEnd/src/app/feature/dashboard/admin/orders/orders.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Response } from '@angular/http';
+
+import 'rxjs/Rx';
+
+import { OrderAdmin } from '../../../../shared/models/dashboard-admin/orders/order-admin.model';
+
+import { OrderService } from '../../../../shared/services/order/order.service';
+
 @Component({
   selector: 'app-orders',
   templateUrl: './orders.component.html',
-  styleUrls: ['./orders.component.scss']
+  styleUrls: ['./orders.component.scss'],
+  providers: [OrderService]
 })
 export class OrdersComponent implements OnInit {
 
-  constructor() { }
+  pendingOrders: OrderAdmin[] = [];
+  acceptedOrders: OrderAdmin[] = [];
+  completedOrders: OrderAdmin[] = [];
+  deniedOrders: OrderAdmin[] = [];
+
+  constructor(private orderService: OrderService) { }
 
   ngOnInit() {
+
+    this.orderService.getOrders()
+      .subscribe( //returns an array
+        (orders) => {
+          const pending = orders.filter(order => order.status === 'pending');
+          this.pendingOrders = pending;
+          const accepted = orders.filter(order => order.status === 'accepted');
+          this.acceptedOrders = accepted;
+          const completed = orders.filter(order => order.status === 'completed');
+          this.completedOrders = completed;
+          const denied = orders.filter(order => order.status === 'denied');
+          this.deniedOrders = denied;
+        }  
+      )
+
   }
 
 }
--- frontEnd/src/app/shared/services/order/order.service.ts
+++ frontEnd/src/app/shared/services/order/order.service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class OrderService {
 
-  constructor() { }
+  url = '../../../../assets/api/ordersAdmin.json';
+
+  constructor(private http: Http) { }
+
+  getOrders() {
+    return this.http.get(this.url)
+      .map(
+        (response: Response) => {
+          const data = response.json();
+          return data;
+        }  
+      )
+  }
 
 }
