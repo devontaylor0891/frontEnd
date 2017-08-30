50500e734da7862789ae87c8ebf489cbd547c0a7
DEVON TAYLOR <devontaylor0891@gmail.com>
2017-08-30 02:34:51 +0000

    Add Auth0 login method

--- frontEnd/src/app/auth/auth.service.ts
+++ frontEnd/src/app/auth/auth.service.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import 'rxjs/add/operator/filter';
+import * as auth0 from 'auth0-js';
 
 @Injectable()
 export class AuthService {
+    
+    auth0 = new auth0.WebAuth({
+        clientID: 'qkYFFjY05abDAnkdLFdZoOdkfZaxFpHk',
+        domain: 'olf.auth0.com',
+        responseType: 'token id_token',
+        audience: 'https://olf.auth0.com/userinfo',
+        redirectUri: 'http://localhost:4200/callback',
+        scope: 'openid'
+    });
 
-  constructor() { }
+  constructor(public router: Router) {}
+      
+      public login(): void {
+          this.auth0.authorize();
+      }
+      
+  
 
 }
