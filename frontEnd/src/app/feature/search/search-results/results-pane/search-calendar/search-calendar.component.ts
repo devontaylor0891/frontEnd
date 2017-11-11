aac7b0009a9e69bd449f76d6819a31fe4579340e
Devon <devontaylor0891@gmail.com>
2017-11-11 14:57:19 -0600

    Add search-calendar.

--- frontEnd/src/app/feature/search/search-results/results-pane/search-calendar/search-calendar.component.ts
+++ frontEnd/src/app/feature/search/search-results/results-pane/search-calendar/search-calendar.component.ts
@@ -1,23 +1,119 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ChangeDetectionStrategy,
+  ViewChild,
+  TemplateRef,
+  OnInit,
+  OnChanges
+} from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+
+import {
+  startOfDay,
+  endOfDay,
+  subDays,
+  addDays,
+  endOfMonth,
+  isSameDay,
+  isSameMonth,
+  addHours
+} from 'date-fns';
+import { Subject } from 'rxjs/Subject';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import {
+  CalendarEvent,
+  CalendarEventAction,
+  CalendarEventTimesChangedEvent
+} from 'angular-calendar';
+
+import { ScheduleModel } from '../../../../../core/models/schedule.model';
 
 import { SearchService } from '../../../../../core/services/search/search.service';
+
+const colors: any = {
+  red: {
+    primary: '#3c910f',
+    secondary: '#FAE3E3'
+  },
+  blue: {
+    primary: '#1e90ff',
+    secondary: '#D1E8FF'
+  },
+  yellow: {
+    primary: '#e3bc08',
+    secondary: '#FDF1BA'
+  }
+};
 
 @Component({
   selector: 'app-search-calendar',
   templateUrl: './search-calendar.component.html',
   styleUrls: ['./search-calendar.component.scss']
 })
-export class SearchCalendarComponent implements OnInit {
+export class SearchCalendarComponent implements OnInit, OnChanges {
 
-  constructor(private searchService: SearchService) { }
+  @ViewChild('modalContent') modalContent: TemplateRef<any>;
+
+  view: string = 'month';
+
+  viewDate: Date = new Date();
+
+  modalData: {
+    action: string;
+    event: CalendarEvent;
+  };
+
+  refresh: Subject<any> = new Subject();
+
+  events: Array<CalendarEvent<{ schedule: any }>> = [];
+
+  activeDayIsOpen: boolean = true;
+
+  dayClicked({ date, events }: { date: Date; events: CalendarEvent[] }): void {
+    if (isSameMonth(date, this.viewDate)) {
+      if (
+        (isSameDay(this.viewDate, date) && this.activeDayIsOpen === true) ||
+        events.length === 0
+      ) {
+        this.activeDayIsOpen = false;
+      } else {
+        this.activeDayIsOpen = true;
+        this.viewDate = date;
+      }
+    }
+  }
+
+  handleEvent(action: string, event: CalendarEvent): void {
+    this.modalData = { event, action };
+    this.modal.open(this.modalContent, { size: 'lg' });
+  }
+  
+  constructor(private modal: NgbModal,
+              private route: ActivatedRoute,
+              private searchService: SearchService) {}
+
+  ngOnChanges() {}
 
   ngOnInit() {
 
+    // subscribe to the get method results
     this.searchService.getDeliveries()
       .subscribe(
-        results => {
-          console.log('from getDeliveries');
-          console.log(results);
+        result => {
+          console.log('result:');
+          console.log(result);
+          let data = result;
+          data.forEach((result) => {
+            this.events.push({
+              title: result.type,
+              color: colors.red,
+              start: new Date(result.startDateTime),
+              meta: {
+                schedule: result
+              }
+            } )
+          })
+          this.refresh.next();
         }
       );
 
