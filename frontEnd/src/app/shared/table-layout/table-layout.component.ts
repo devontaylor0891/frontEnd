eb1d7b87bfe5b9197fc6e3257950f4479eb4951d
Devon <devontaylor0891@gmail.com>
2017-12-11 09:17:59 -0600

    Small lint changes.

--- frontEnd/src/app/shared/table-layout/table-layout.component.ts
+++ frontEnd/src/app/shared/table-layout/table-layout.component.ts
@@ -27,16 +27,15 @@ export class TableLayoutComponent implements OnInit, OnChanges {
   ngOnChanges() {
     if (this.settings) { // when settings provided
       this.columnMaps = this.settings;
-  } else { // no settings, create column maps with defaults
+    } else { // no settings, create column maps with defaults
       this.columnMaps = Object.keys(this.records[0])
-          .map( key => {
-               return {
-                   primaryKey: key,
-                   header: key.slice(0, 1).toUpperCase() + 
-                      key.replace(/_/g, ' ' ).slice(1)
-          }
+        .map( key => {
+          return {
+            primaryKey: key,
+            header: key.slice(0, 1).toUpperCase() + key.replace(/_/g, ' ' ).slice(1)
+          };
       });
-  }
+    }
   }
 
   constructor(private modal: NgbModal) { }
