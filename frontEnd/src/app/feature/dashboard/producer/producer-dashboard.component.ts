c8193ce640831f38ac6501dc1b7cbbb243301477
Devon <devontaylor0891@gmail.com>
2017-12-17 21:26:14 -0600

    Add producer dash service

--- frontEnd/src/app/feature/dashboard/producer/producer-dashboard.component.ts
+++ frontEnd/src/app/feature/dashboard/producer/producer-dashboard.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ProducerDashboardService } from '../producer-dashboard.service';
+
 @Component({
   selector: 'app-producer-dashboard',
   templateUrl: './producer-dashboard.component.html',
-  styleUrls: ['./producer-dashboard.component.scss']
+  styleUrls: ['./producer-dashboard.component.scss'],
+  providers: [ProducerDashboardService]
 })
 export class ProducerDashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dashboard: ProducerDashboardService) { }
 
   ngOnInit() {
   }
