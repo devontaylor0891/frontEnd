438953d4600a15ce98071cbee21609623c9c89f8
Devon <devontaylor0891@gmail.com>
2018-04-01 20:40:20 -0600

    Working on update profile page to make form reactive. Not working yet.

--- frontEnd/src/app/auth/update-profile/update-profile.component.ts
+++ frontEnd/src/app/auth/update-profile/update-profile.component.ts
@@ -36,6 +36,7 @@
 //   lng: number;
 //   streetNumber: number;
 //   route: any;
+//   address: string;
 //   city: string;
 //   province: string;
 //   postalCode: string;
@@ -49,7 +50,27 @@
 //               private userService: UserService,
 //               private router: Router,
 //               private mapsAPILoader: MapsAPILoader,
-//               private ngZone: NgZone) { }
+//               private ngZone: NgZone) {
+
+//     // build an empty submitObject
+//     this.submitObject = {
+//       id: null,
+//       name: '',
+//       location: '',
+//       province: '',
+//       address: '',
+//       description: '',
+//       email: '',
+//       logoUrl: '',
+//       longitude: null,
+//       latitude: null,
+//       firstName: '',
+//       status: ''
+//       // productList: [],
+//       // scheduleList: []
+//     }
+
+//   }
 
 //   ngOnChanges() {}
 
@@ -135,7 +156,7 @@
 //             this.userService.getUserFromDb(this.id);
 //           }
 //         )
-//     }
+//     };
 //     if (form.role === 'producer') {
 //       // get and patch user data
 //       this.newValues = {
@@ -143,32 +164,47 @@
 //         email: form.email,
 //         role: form.role
 //       };
+//       this.buildSubmitObject(form);
+//       console.log('submitObject built: ', this.submitObject);
 //       this.apiService.patchUser(this.id, this.newValues)
 //         .subscribe(
 //           result => {
-//             // on success, mark the profile as complete
-//             console.log('user updated: ', result);
-//             this.userService.profileIncomplete$.next(false);
-//             this.userService.getUserFromDb(this.id);
+//             console.log('user/producer updated: ', result);
+//             // get and post producer data
+//             this.buildSubmitObject(form);
+//             this.apiService.createProducer(this.submitObject)
+//               .subscribe(
+//                 result => {
+//                   console.log('producer profile created: ', result);
+//                   // on success, mark the profile as complete
+//                   this.userService.profileIncomplete$.next(false);
+//                   this.userService.getUserFromDb(this.id);
+//                 }
+//               );
 //           }
 //         );
-//       // get and post producer data
-//       this.newProducerValues = {
-//         name: form.name,
-//         location: form.location,
-//         province: form.province,
-//         address: form.address,
-//         description: form.description,
-//         email: form.email,
-//         logoUrl: form.logoUrl,
-//         longitude: form.longitude,
-//         latitude: form.latitude,
-//         firstName: form.firstName,
-//         status: 'active'
-//       };
-//       console.log('new Producer: ', this.newProducerValues);
+//     }; 
+//   };
+
+//   private buildSubmitObject(form) {
+//     this.submitObject = {
+//       id: this.id,
+//       name: form.name,
+//       location: this.city,
+//       province: this.province,
+//       address: this.address,
+//       description: form.description,
+//       email: form.email,
+//       logoUrl: form.logoUrl,
+//       longitude: this.longitude,
+//       latitude: this.latitude,
+//       firstName: form.firstName,
+//       status: 'active',
+//       // productList: [],
+//       // scheduleList: [],
+//       products: [],
+//       schedule: []
 //     }
-    
 //   }
 
 //   private fillAddress(place) {
@@ -178,12 +214,10 @@
 //     this.lat = place.geometry.location.lat();
 //     this.lng = place.geometry.location.lng();
 //     if (this.streetNumber && this.route) {
-//       this.submitObject.address = this.streetNumber + ' ' + this.route;
+//       this.address = this.streetNumber + ' ' + this.route;
 //     };
-//     this.submitObject.location = this.city; // still not working
-//     this.submitObject.province = this.province;
-//     this.submitObject.latitude = this.lat;
-//     this.submitObject.longitude = this.lng;
+//     this.latitude = this.lat;
+//     this.longitude = this.lng;
 //   };
 
 //   private parseAddressComponents(components) {
@@ -217,6 +251,7 @@
 
 // }
 
+
 import { Component, OnInit, OnChanges, NgZone, ViewChild, ElementRef } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
@@ -244,11 +279,17 @@ export class UpdateProfileComponent implements OnInit, OnChanges {
   newValues: Object;
   newProducerValues: Object;
 
+  // reactive form
+  userForm: FormGroup;
+
+  // to transition the form between consumer and producer
+  role: string;
+
   public latitude: number;
   public longitude: number;
   public searchControl: FormControl;
   public zoom: number;
-  @ViewChild("search") public searchElementRef: ElementRef;  
+  @ViewChild('search') public searchElementRef: ElementRef;
 
   @ViewChild(AgmMap) public agmMap: AgmMap;
   lat: number;
@@ -262,32 +303,13 @@ export class UpdateProfileComponent implements OnInit, OnChanges {
   country: string;
   submitObject: ProducerModel;
 
-  role: string;
-
   constructor(private auth: AuthService,
               private apiService: ApiService,
               private userService: UserService,
               private router: Router,
               private mapsAPILoader: MapsAPILoader,
-              private ngZone: NgZone) {
-
-    // build an empty submitObject
-    this.submitObject = {
-      id: null,
-      name: '',
-      location: '',
-      province: '',
-      address: '',
-      description: '',
-      email: '',
-      logoUrl: '',
-      longitude: null,
-      latitude: null,
-      firstName: '',
-      status: ''
-      // productList: [],
-      // scheduleList: []
-    }
+              private ngZone: NgZone,
+              private fb: FormBuilder) {
 
   }
 
@@ -300,25 +322,25 @@ export class UpdateProfileComponent implements OnInit, OnChanges {
   ngOnInit() { 
 
     // ***** LOCATION INIT CODE ******
-    //set google maps defaults
+    // set google maps defaults
     this.zoom = 8;
     this.latitude = 50.5555;
     this.longitude = -100.5555;
 
-    //create search FormControl
+    // create search FormControl
     this.searchControl = new FormControl();
 
-    //set current position
+    // set current position
     // this.setCurrentPosition();
 
-    //load Places Autocomplete
+    // load Places Autocomplete
     this.mapsAPILoader.load().then(() => {
       let autocomplete = new google.maps.places.Autocomplete(this.searchElementRef.nativeElement, {
         types: ["geocode"]
       });
       autocomplete.addListener("place_changed", () => {
         this.ngZone.run(() => {
-          //get the place result
+          // get the place result
           let place: google.maps.places.PlaceResult = autocomplete.getPlace();
 
           // verify result
@@ -337,6 +359,7 @@ export class UpdateProfileComponent implements OnInit, OnChanges {
       });
     });
 
+    // get the basic info for the user
     this.auth.getParsedId()
       .subscribe(
         result => {
@@ -351,7 +374,7 @@ export class UpdateProfileComponent implements OnInit, OnChanges {
           console.log('firstName: ', this.firstName);
         }
       );
-    
+
     this.userService.getEmail()
       .subscribe(
         result => {
@@ -359,72 +382,91 @@ export class UpdateProfileComponent implements OnInit, OnChanges {
         }
       );
 
+    // instantiate the formgroup
+    this.userForm = new FormGroup({
+      user: new FormGroup({
+        firstName: new FormControl('', [Validators.required]),
+        email: new FormControl('', [Validators.required]),
+        role: new FormControl('consumer', [Validators.required])
+      }),
+      producer: new FormGroup({
+        id: new FormControl(this.id),
+        name: new FormControl('', [Validators.required]),
+        location: new FormControl('', [Validators.required]),
+        province: new FormControl('', [Validators.required]),
+        address: new FormControl(''),
+        description: new FormControl('', [Validators.required]),
+        logoUrl: new FormControl(''),
+        longitude: new FormControl('', [Validators.required]),
+        latitude: new FormControl('', [Validators.required])
+      }),
+      status: new FormControl('active')
+    });
+
+    // set producer fields to disabled
+    this.disableProducerFields();
+
   }
-  
+
+  disableProducerFields() {
+    this.userForm.controls.producer.disable();
+    // this.userForm.controls.name.disable();// disables the specified control, which removes it from validation, thus preventing false flags.
+    // this.userForm.controls.location.disable();
+    // this.userForm.controls.province.disable();
+    // this.userForm.controls.address.disable();
+    // this.userForm.controls.description.disable();
+    // this.userForm.controls.logoUrl.disable();
+    // this.userForm.controls.longitude.disable();
+    // this.userForm.controls.latitude.disable();
+    // this.userForm.controls.status.disable();
+  };
+
+  enableProducerFields() {
+    this.userForm.controls.producer.enable();
+    // this.userForm.controls.name.enable();// disables the specified control, which removes it from validation, thus preventing false flags.
+    // this.userForm.controls.location.enable();
+    // this.userForm.controls.province.enable();
+    // this.userForm.controls.address.enable();
+    // this.userForm.controls.description.enable();
+    // this.userForm.controls.logoUrl.enable();
+    // this.userForm.controls.longitude.enable();
+    // this.userForm.controls.latitude.enable();
+    // this.userForm.controls.status.enable();
+  };
+
+  onSelectConsumer() {
+    this.role = 'consumer';
+    this.userForm.patchValue({user:{role: 'consumer'}});
+    this.disableProducerFields();
+  };
+
+  onSelectProducer() {
+    this.role = 'producer';
+    this.userForm.patchValue({user:{role: 'producer'}});
+    this.enableProducerFields();
+  };
+
   onSubmit(form: any): void {
-    console.log('form value: ', form);
+    console.log('form value: ', form.value);
     console.log(this.id);
-    if (form.role === 'consumer') {
-      this.newValues = form;
-      this.apiService.patchUser(this.id, this.newValues)
-        .subscribe(
-          result => {
-            // on success, mark the profile as complete
-            console.log('user updated: ', result);
-            this.userService.profileIncomplete$.next(false);
-            this.userService.getUserFromDb(this.id);
-          }
-        )
-    };
-    if (form.role === 'producer') {
-      // get and patch user data
-      this.newValues = {
-        firstName: form.firstName,
-        email: form.email,
-        role: form.role
-      };
-      this.buildSubmitObject(form);
-      console.log('submitObject built: ', this.submitObject);
-      this.apiService.patchUser(this.id, this.newValues)
-        .subscribe(
-          result => {
-            console.log('user/producer updated: ', result);
-            // get and post producer data
-            this.buildSubmitObject(form);
-            this.apiService.createProducer(this.submitObject)
+    this.apiService.patchUser(this.id, form.value.user)
+      .subscribe(
+        result => {
+          console.log('user updated: ', result);
+          if (form.value.role === 'producer') {
+            this.apiService.createProducer(form.value.producer)
               .subscribe(
                 result => {
                   console.log('producer profile created: ', result);
-                  // on success, mark the profile as complete
-                  this.userService.profileIncomplete$.next(false);
-                  this.userService.getUserFromDb(this.id);
                 }
               );
-          }
-        );
-    }; 
+          };
+          // mark profile complete and get the full profile
+          this.userService.profileIncomplete$.next(false);
+          this.userService.getUserFromDb(this.id);
+        }
+      );
   };
-
-  private buildSubmitObject(form) {
-    this.submitObject = {
-      id: this.id,
-      name: form.name,
-      location: this.city,
-      province: this.province,
-      address: this.address,
-      description: form.description,
-      email: form.email,
-      logoUrl: form.logoUrl,
-      longitude: this.longitude,
-      latitude: this.latitude,
-      firstName: form.firstName,
-      status: 'active',
-      // productList: [],
-      // scheduleList: [],
-      products: [],
-      schedule: []
-    }
-  }
 
   private fillAddress(place) {
     // this.clearAddress();
