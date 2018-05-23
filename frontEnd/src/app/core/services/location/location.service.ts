701b44b114025df973506453d470e53d431df57a
devontaylor0891 <devontaylor0891@gmail.com>
2018-05-22 21:25:22 -0600

    Update location in search options displaying properly.

--- frontEnd/src/app/core/services/location/location.service.ts
+++ frontEnd/src/app/core/services/location/location.service.ts
@@ -1,31 +1,158 @@
+// // // provided in App Module
+
+// // // called in ...
+// // // Table Layout Comp
+
+// // import { Injectable } from '@angular/core';
+// // import { Observable } from 'rxjs/Observable';
+
+// // @Injectable()
+// // export class LocationService {
+
+// //   constructor() { }
+
+// //   getLocation(): Observable<any> {
+// //     return Observable.create(observer => {
+// //         if(window.navigator && window.navigator.geolocation) {
+// //             window.navigator.geolocation.getCurrentPosition(
+// //                 (position) => {
+// //                     observer.next(position);
+// //                     observer.complete();
+// //                 },
+// //                 (error) => observer.error(error)
+// //             );
+// //         } else {
+// //             observer.error('Unsupported Browser');
+// //         }
+// //     });
+// // }
+
+// // }
+
 // // provided in App Module
 
 // // called in ...
 // // Table Layout Comp
+// // Search comp
 
-// import { Injectable } from '@angular/core';
+// import { Injectable, NgZone } from '@angular/core';
 // import { Observable } from 'rxjs/Observable';
+// import { BehaviorSubject } from 'rxjs';
+
+// import { } from 'googlemaps';
+// import { MapsAPILoader } from '@agm/core';
+// import { AgmMap } from '@agm/core';
+
 
 // @Injectable()
 // export class LocationService {
 
-//   constructor() { }
+//     geocoder: google.maps.Geocoder;
+//     lat: any;
+//     lng: any;
+//     city: string;
+//     province: string;
+//     cityProvince: string = '';
+//     location: any;
+//     results: any;
+//     geoCoderResults: any = {};
 
-//   getLocation(): Observable<any> {
-//     return Observable.create(observer => {
-//         if(window.navigator && window.navigator.geolocation) {
-//             window.navigator.geolocation.getCurrentPosition(
-//                 (position) => {
-//                     observer.next(position);
-//                     observer.complete();
-//                 },
-//                 (error) => observer.error(error)
-//             );
-//         } else {
-//             observer.error('Unsupported Browser');
+//     _cityProvince: BehaviorSubject<string>;
+
+//     constructor(private mapsAPILoader: MapsAPILoader,
+//                 private ngZone: NgZone) {
+
+//         this._cityProvince = <BehaviorSubject<string>>new BehaviorSubject('');
+
+//     }
+
+//     getLocation(): Observable<any> {
+//         return Observable.create(observer => {
+//             if (window.navigator && window.navigator.geolocation) {
+//                 window.navigator.geolocation.getCurrentPosition(
+//                     (position) => {
+//                         this.lat = position.coords.latitude;
+//                         this.lng = position.coords.longitude;
+//                         // console.log('position: ', position);
+//                         observer.next(position);
+//                         observer.complete();
+//                     },
+//                     (error) => observer.error(error)
+//                 );
+//             } else {
+//                 observer.error('Unsupported Browser');
+//             }
+//         });
+//     };
+
+//     getCityProvince(): Observable<string> {
+//         return this._cityProvince.asObservable();
+//     };
+
+
+//     codeLatLng(lat, lng) {
+//         this.mapsAPILoader.load().then(() => {
+//             this.geocoder = new google.maps.Geocoder();
+//             let latlng = new google.maps.LatLng(lat, lng);
+//             let self = this;
+//             this.results = this.geocoder.geocode({'location': latlng}, function(results, status) {
+//                 if (status === google.maps.GeocoderStatus.OK) {
+//                     self.geoCoderResults = results;
+//                     console.log('geocoerresults: ', self);
+//                     // if (results[1]) {
+//                     //     // this.fillCityProvince(results[1]);
+//                     //     let components = results[1].address_components;
+//                     //     for (let i = 0; i < components.length; i++) {
+//                     //         let types = components[i].types;
+//                     //         for (let j = 0; j < types.length; j++) {
+//                     //           let result = types[j];
+//                     //           if (result === 'locality' || result === 'sublocality') {
+//                     //             this.city = components[i].short_name;
+//                     //           }
+//                     //           if (result === 'administrative_area_level_1') {
+//                     //             this.province = components[i].short_name;
+//                     //           }
+//                     //           this.cityProvince = this.city + ', ' + this.province;
+//                     //         //   this.ngZone.run(() => {
+//                     //         //       this.cityProvince = components[i].short_name + ', ' + components[i].short_name;
+//                     //         //       this._cityProvince.next(this.cityProvince);
+//                     //         //   });
+//                     //         }
+//                     //       }
+//                     //     // this.cityProvince = this.city + ', ' + this.province;
+//                     //     // console.log('cityprovince:', this.cityProvince);
+//                     // } else {
+//                     // alert("No results found");
+//                     // };
+//                     // // this._cityProvince.next(this.cityProvince);
+//                     // // console.log('new value out: ', this.cityProvince);
+//                     // return results;
+//                 } else {
+//                     alert("Geocoder failed due to: " + status);
+//                 };
+//             });
+//         });
+//     };
+
+//     fillCityProvince(results) {
+//         console.log('fill reached:', results);
+//         let components = results.address_components;
+//         for (let i = 0; i < components.length; i++) {
+//             let types = components[i].types;
+//             for (let j = 0; j < types.length; j++) {
+//                 let result = types[j];
+//                 if (result === 'locality' || result === 'sublocality') {
+//                 this.city = components[i].short_name;
+//                 }
+//                 if (result === 'administrative_area_level_1') {
+//                 this.province = components[i].short_name;
+//                 }
+//                 this.cityProvince = this.city + ', ' + this.province;
+//                 console.log('cityprovince:', this.cityProvince);
+//                 this._cityProvince.next(this.cityProvince);
+//             }
 //         }
-//     });
-// }
+//     }
 
 // }
 
@@ -34,6 +161,7 @@
 // called in ...
 // Table Layout Comp
 // Search comp
+// search options comp
 
 import { Injectable, NgZone } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
@@ -89,7 +217,6 @@ export class LocationService {
         return this._cityProvince.asObservable();
     };
 
-
     codeLatLng(lat, lng) {
         this.mapsAPILoader.load().then(() => {
             this.geocoder = new google.maps.Geocoder();
@@ -99,34 +226,31 @@ export class LocationService {
                 if (status === google.maps.GeocoderStatus.OK) {
                     self.geoCoderResults = results;
                     console.log('geocoerresults: ', self);
-                    // if (results[1]) {
-                    //     // this.fillCityProvince(results[1]);
-                    //     let components = results[1].address_components;
-                    //     for (let i = 0; i < components.length; i++) {
-                    //         let types = components[i].types;
-                    //         for (let j = 0; j < types.length; j++) {
-                    //           let result = types[j];
-                    //           if (result === 'locality' || result === 'sublocality') {
-                    //             this.city = components[i].short_name;
-                    //           }
-                    //           if (result === 'administrative_area_level_1') {
-                    //             this.province = components[i].short_name;
-                    //           }
-                    //           this.cityProvince = this.city + ', ' + this.province;
-                    //         //   this.ngZone.run(() => {
-                    //         //       this.cityProvince = components[i].short_name + ', ' + components[i].short_name;
-                    //         //       this._cityProvince.next(this.cityProvince);
-                    //         //   });
-                    //         }
-                    //       }
-                    //     // this.cityProvince = this.city + ', ' + this.province;
-                    //     // console.log('cityprovince:', this.cityProvince);
-                    // } else {
-                    // alert("No results found");
-                    // };
-                    // // this._cityProvince.next(this.cityProvince);
-                    // // console.log('new value out: ', this.cityProvince);
-                    // return results;
+                    if (results[1]) {
+                        let components = results[1].address_components;
+                        for (let i = 0; i < components.length; i++) {
+                            let types = components[i].types;
+                            for (let j = 0; j < types.length; j++) {
+                              let result = types[j];
+                              if (result === 'locality' || result === 'sublocality') {
+                                this.city = components[i].short_name;
+                              }
+                              if (result === 'administrative_area_level_1') {
+                                this.province = components[i].short_name;
+                              }
+                              self.cityProvince = this.city + ', ' + this.province;
+                              self.ngZone.run(() => {
+                                self._cityProvince.next(self.cityProvince);
+                              });
+                            //   this.ngZone.run(() => {
+                            //       this.cityProvince = components[i].short_name + ', ' + components[i].short_name;
+                            //       this._cityProvince.next(this.cityProvince);
+                            //   });
+                            }
+                          }
+                    } else {
+                    alert("No results found");
+                    };
                 } else {
                     alert("Geocoder failed due to: " + status);
                 };
@@ -134,24 +258,9 @@ export class LocationService {
         });
     };
 
-    fillCityProvince(results) {
-        console.log('fill reached:', results);
-        let components = results.address_components;
-        for (let i = 0; i < components.length; i++) {
-            let types = components[i].types;
-            for (let j = 0; j < types.length; j++) {
-                let result = types[j];
-                if (result === 'locality' || result === 'sublocality') {
-                this.city = components[i].short_name;
-                }
-                if (result === 'administrative_area_level_1') {
-                this.province = components[i].short_name;
-                }
-                this.cityProvince = this.city + ', ' + this.province;
-                console.log('cityprovince:', this.cityProvince);
-                this._cityProvince.next(this.cityProvince);
-            }
-        }
+    updateCityProvince(cityProvinceString) {
+        this.cityProvince = cityProvinceString;
+        this._cityProvince.next(this.cityProvince);
     }
 
 }
--- frontEnd/src/app/feature/search/search-options/search-options/search-options.component.ts
+++ frontEnd/src/app/feature/search/search-options/search-options/search-options.component.ts
@@ -1,14 +1,136 @@
-// import { Component, OnInit, OnChanges } from '@angular/core';
+// // import { Component, OnInit, OnChanges } from '@angular/core';
+// // import { NgForm } from '@angular/forms';
+
+// // import { SearchService } from '../../../../core/services/search/search.service';
+
+// // @Component({
+// //   selector: 'app-search-options',
+// //   templateUrl: './search-options.component.html',
+// //   styleUrls: ['./search-options.component.scss']
+// // })
+// // export class SearchOptionsComponent implements OnInit, OnChanges {
+
+// //   deliveryTypes: any[];
+// //   categoriesList: any[];
+// //   submittedValues: any = {
+// //     categories: [],
+// //     deliveryTypes: []
+// //   };
+
+// //   constructor(private searchService: SearchService) {
+
+// //     this.deliveryTypes = [
+// //       {
+// //         type: '',
+// //         checkboxValue: null
+// //       }
+// //     ];
+
+// //     this.categoriesList = [
+// //       {
+// //         category: '',
+// //         checkboxValue: null
+// //       }
+// //     ];
+
+// //   }
+
+// //   ngOnChanges() {
+
+// //   }
+
+// //   ngOnInit() {
+
+// //     // subscribe to the delivery types
+// //     this.searchService.getDeliveryTypes()
+// //       .subscribe(
+// //         results => {
+// //           for (let i = 0; i < results.length; i++) {
+// //             let newDel = {
+// //               type: results[i],
+// //               checkboxValue: true
+// //             };
+// //             this.deliveryTypes[i] = newDel;
+// //           };
+// //         //  this.deliveryTypes = result;
+// //         //  console.log("These are the delivery types from the subscription:");
+// //         //  console.log(this.deliveryTypes);
+// //         }
+// //       );
+
+// //     // subscribe to the category types
+// //     this.searchService.getCategories()
+// //       .subscribe(
+// //         results => {
+// //           for (let i = 0; i < results.length; i++) {
+// //             let newCat = {
+// //               category: results[i],
+// //               checkboxValue: true
+// //             };
+// //             this.categoriesList[i] = newCat;
+// //           };
+// //           //this.categoriesList = results;
+// //         }
+// //       );
+
+// //   }
+
+// //   onSubmit(form: NgForm) {
+// //     //empty the submitted values
+// //     this.submittedValues = {
+// //       categories: [],
+// //       deliveryTypes: []
+// //     };
+// //     // separate and loop through each of the values
+// //     for (let property in form.value) {
+// //       if (form.value.hasOwnProperty(property)) {
+// //         let propValue = form.value[property]; //get the returned values
+// //         // if the returned value is true
+// //         if (propValue) {
+// //           //separate the properties by their type (category or delivery)
+// //           if (property.split('.')[0] === 'c') {
+// //             //add the property to the appropriate array
+// //             this.submittedValues.categories.push(property.split('.')[1]);
+// //           } else {
+// //             this.submittedValues.deliveryTypes.push(property.split('.')[1]);
+// //           }
+// //         }
+// //       }
+// //     }
+// //     //then send the submitted values to search service to update the view
+// //     this.searchService.onFilter(this.submittedValues);
+// //   }
+
+// //   reset(form: NgForm) {
+// //     // reset all checkboxes to true
+// //     for (let i = 0; i < this.deliveryTypes.length; i++) {
+// //       this.deliveryTypes[i].checkboxValue = true;
+// //     }
+// //     for (let i = 0; i < this.categoriesList.length; i++) {
+// //       this.categoriesList[i].checkboxValue = true;
+// //     }
+// //     this.searchService.onReset();
+// //     this.submittedValues = {
+// //       categories: [],
+// //       deliveryTypes: []
+// //     };
+// //   }
+
+// // }
+
+
+// import { Component, OnInit, Input } from '@angular/core';
 // import { NgForm } from '@angular/forms';
 
 // import { SearchService } from '../../../../core/services/search/search.service';
+// import { LocationService } from '../../../../core/services/location/location.service';
 
 // @Component({
 //   selector: 'app-search-options',
 //   templateUrl: './search-options.component.html',
 //   styleUrls: ['./search-options.component.scss']
 // })
-// export class SearchOptionsComponent implements OnInit, OnChanges {
+// export class SearchOptionsComponent implements OnInit {
 
 //   deliveryTypes: any[];
 //   categoriesList: any[];
@@ -17,7 +139,15 @@
 //     deliveryTypes: []
 //   };
 
-//   constructor(private searchService: SearchService) {
+//   submitting: boolean = false;
+//   currentRadius: number = 25;
+//   latitude: number;
+//   longitude: number;
+
+//   city: string;
+
+//   constructor(private searchService: SearchService,
+//               private locationService: LocationService) {
 
 //     this.deliveryTypes = [
 //       {
@@ -35,10 +165,6 @@
 
 //   }
 
-//   ngOnChanges() {
-
-//   }
-
 //   ngOnInit() {
 
 //     // subscribe to the delivery types
@@ -52,9 +178,6 @@
 //             };
 //             this.deliveryTypes[i] = newDel;
 //           };
-//         //  this.deliveryTypes = result;
-//         //  console.log("These are the delivery types from the subscription:");
-//         //  console.log(this.deliveryTypes);
 //         }
 //       );
 
@@ -69,7 +192,13 @@
 //             };
 //             this.categoriesList[i] = newCat;
 //           };
-//           //this.categoriesList = results;
+//         }
+//       );
+
+//     this.locationService.getCityProvince()
+//       .subscribe(
+//         results => {
+//           this.city = results;
 //         }
 //       );
 
@@ -114,13 +243,31 @@
 //       categories: [],
 //       deliveryTypes: []
 //     };
+//   };
+
+//   onRadiusSubmit(radius: number) {
+//     this.submitting = true;
+//     if (this.currentRadius === radius) {
+//       this.submitting = false;
+//       return;
+//     } else {
+//       this.searchService.loadAll(this.latitude, this.longitude, radius);
+//     }
+
+//   };
+
+//   changeCity() {
+//     console.log('need to change city please');
 //   }
 
 // }
 
+import { Component, OnInit, Input, NgZone, ViewChild, ElementRef } from '@angular/core';
+import { NgForm, FormControl, FormGroup } from '@angular/forms';
 
-import { Component, OnInit, Input } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { } from 'googlemaps';
+import { MapsAPILoader } from '@agm/core';
+import { AgmMap } from '@agm/core';
 
 import { SearchService } from '../../../../core/services/search/search.service';
 import { LocationService } from '../../../../core/services/location/location.service';
@@ -139,15 +286,29 @@ export class SearchOptionsComponent implements OnInit {
     deliveryTypes: []
   };
 
+  locationUpdate: boolean = false;
   submitting: boolean = false;
   currentRadius: number = 25;
   latitude: number;
   longitude: number;
-
+  cityProvince: string;
+ 
+  streetNumber: number;
+  route: any;
+  address: string;
   city: string;
+  province: string;
+  postalCode: string;
+  country: string;
+
+  public searchControl: FormControl;
+  @ViewChild('search') public searchElementRef: ElementRef;
+  zoom: number;
 
   constructor(private searchService: SearchService,
-              private locationService: LocationService) {
+              private locationService: LocationService,
+              private mapsAPILoader: MapsAPILoader,
+              private ngZone: NgZone) {
 
     this.deliveryTypes = [
       {
@@ -163,7 +324,7 @@ export class SearchOptionsComponent implements OnInit {
       }
     ];
 
-  }
+  };
 
   ngOnInit() {
 
@@ -198,9 +359,41 @@ export class SearchOptionsComponent implements OnInit {
     this.locationService.getCityProvince()
       .subscribe(
         results => {
-          this.city = results;
+          this.cityProvince = results;
         }
       );
+
+    // create search FormControl
+    this.searchControl = new FormControl('');
+
+
+    // load Places Autocomplete
+    console.log('searchelement: ', this.searchElementRef);
+    this.mapsAPILoader.load().then(() => {
+      console.log('searchelement: ', this.searchElementRef);
+      let autocomplete = new google.maps.places.Autocomplete(this.searchElementRef.nativeElement, {
+        types: ["geocode"]
+      });
+      autocomplete.addListener("place_changed", () => {
+        this.ngZone.run(() => {
+          // get the place result
+          let place: google.maps.places.PlaceResult = autocomplete.getPlace();
+
+          // verify result
+          if (place.geometry === undefined || place.geometry === null) {
+            return;
+          }
+
+          console.log('place: ', place);
+          this.fillAddress(place);
+
+          // set latitude, longitude and zoom
+          this.latitude = place.geometry.location.lat();
+          this.longitude = place.geometry.location.lng();
+          this.zoom = 12;
+        });
+      });
+    });
 
   }
 
@@ -258,6 +451,66 @@ export class SearchOptionsComponent implements OnInit {
 
   changeCity() {
     console.log('need to change city please');
-  }
+    this.locationUpdate = !this.locationUpdate;
+    // this.runMapLoader();
+  };
+
+  runMapLoader() {
+    // set current position
+    // this.setCurrentPosition();
+
+    // // load Places Autocomplete
+    // console.log('searchelement: ', this.searchElementRef);
+    // this.mapsAPILoader.load().then(() => {
+    //   console.log('searchelement: ', this.searchElementRef);
+    //   let autocomplete = new google.maps.places.Autocomplete(this.searchElementRef.nativeElement, {
+    //     types: ["geocode"]
+    //   });
+    //   autocomplete.addListener("place_changed", () => {
+    //     this.ngZone.run(() => {
+    //       // get the place result
+    //       let place: google.maps.places.PlaceResult = autocomplete.getPlace();
+
+    //       // verify result
+    //       if (place.geometry === undefined || place.geometry === null) {
+    //         return;
+    //       }
+
+    //       console.log('place: ', place);
+    //       this.fillAddress(place);
+
+    //       // set latitude, longitude and zoom
+    //       this.latitude = place.geometry.location.lat();
+    //       this.longitude = place.geometry.location.lng();
+    //       this.zoom = 12;
+    //     });
+    //   });
+    // });
+  };
+
+  private fillAddress(place) {
+    // this.clearAddress();
+    this.parseAddressComponents(place.address_components);
+    this.latitude = place.geometry.location.lat();
+    this.longitude = place.geometry.location.lng();
+    this.locationService.updateCityProvince(this.city + ', ' + this.province);
+    this.locationUpdate = !this.locationUpdate;
+  };
+
+  private parseAddressComponents(components) {
+    for (let i = 0; i < components.length; i++) {
+      let types = components[i].types;
+      for (let j = 0; j < types.length; j++) {
+        let result = types[j];
+        if (result === 'locality' || result === 'sublocality') {
+          this.city = components[i].short_name;
+          console.log('city: ', this.city);
+        };
+        if (result === 'administrative_area_level_1') {
+          this.province = components[i].short_name;
+        };
+      };
+    };
+  };
 
 }
