0e27855592b213c1bc3482fe6f53836a713c8da7
devontaylor0891 <devontaylor0891@gmail.com>
2018-10-31 21:35:10 -0600

    Fixed csv download.

--- frontEnd/src/app/core/services/utility/utility.service.ts
+++ frontEnd/src/app/core/services/utility/utility.service.ts
@@ -11,30 +11,51 @@ export class UtilityService {
   constructor() { }
 
   ConvertToCSV(objArray) {
+    console.log('type of array: ', typeof objArray);
+    console.log('objArray 0: ', objArray[0]);
     let array = typeof objArray !== 'object' ? JSON.parse(objArray) : objArray;
+    var jsonObject = JSON.stringify(objArray);
+    // let array = JSON.parse(objArray);
     let str = '';
     let row = '';
 
-    for (let index of objArray[0]) {
-      // Now convert each value to string and comma-separated
-      row += index + ',';
-    };
+    console.log(Object.keys(objArray[0]));
+
+    let keys = Object.keys(objArray[0]);
+      keys.forEach(name => {
+        row += name + ',';
+      })
+
+    // for (let index of objArray[0]) {
+    //   console.log('index of objarray: ', index);
+    //   // Now convert each value to string and comma-separated
+    //   row += index + ',';
+    // };
     row = row.slice(0, -1);
+    console.log('row: ', row);
     // append Label row with line break
     str += row + '\r\n';
-
-    for (let i = 0; i < array.length; i++) {
+    console.log('objArray length: ', objArray.length);
+    console.log('array length: ', array);
+    let values;
+    for (let i = 0; i < objArray.length; i++) {
       let line = '';
-      for (let index of array[i]) {
-        if (line !== '') { line += ','};
-        line += array[i][index];
-      }
-      str += line + '\r\n';
+      console.log('objectArr: ', objArray[i]);
+      let lineValues = Object.keys(objArray[i]).map(key => objArray[i][key]);
+      let commaJoinedValues = lineValues.join(",");
+      str += commaJoinedValues + '\r\n';
+      // for (let value of objArray[i]) {
+      //   console.log('value: ', value);
+      //   if (line !== '') { line += ','};
+      //   line += objArray[i][value];
+      // }
+      // str += line + '\r\n';
     }
     return str;
   };
 
-  convertAndDownload(yourData){
+  convertAndDownload(yourData) {
+    console.log('data received: ', yourData);
     let csvData = this.ConvertToCSV(yourData);
     let a = document.createElement('a');
     a.setAttribute('style', 'display:none;');
