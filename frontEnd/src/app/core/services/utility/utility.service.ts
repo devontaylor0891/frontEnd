cd06091344b93b8f2edbe608e207e30c3620eb82
devontaylor0891 <devontaylor0891@gmail.com>
2018-11-06 19:12:24 -0600

    Fixed utility service product and sched to csv.

--- frontEnd/src/app/core/services/utility/utility.service.ts
+++ frontEnd/src/app/core/services/utility/utility.service.ts
@@ -88,40 +88,138 @@
 
 //   ConvertOrderToCSV(objArray) {
 //     let array = typeof objArray !== 'object' ? JSON.parse(objArray) : objArray;
-//     let str = '';
+//     let returnString = '';
 //     let row = '';
+    
+//     // create the header row, append to returnString
+//     let headers = 'ID,Order Date,Order Status,Consumer Name,Consumer Comment,Producer Comment,Total Order Value,Schedule Type,Schedule Date,City,Province,Address,Fee,Fee Waiver,Product Name,Quantity,Product Total Cost';
 
-//     let keys = Object.keys(objArray[0]);
-//       keys.forEach(name => {
-//         row += name + ',';
-//       })
+//     // let keys = Object.keys(objArray[0]);
+//     //   keys.forEach(name => {
+//     //     row += name + ',';
+//     //   })
 
-//     row = row.slice(0, -1);
-//     console.log('row: ', row);
+//     // row = row.slice(0, -1);
+//     // console.log('row: ', row);  // **********************************************
 //     // append Label row with line break
-//     str += row + '\r\n';
-//     console.log('array: ', array);
-//     let values;
-//     for (let i = 0; i < objArray.length; i++) {
-//       let line = '';
-//       console.log('objectArr: ', objArray[i]);
-//       let lineValues = Object.keys(objArray[i]).map(key => objArray[i][key]);
+//     // returnString += row + '\r\n';
+//     returnString += headers + '\r\n';
 
-//       for (let j = 0; j < lineValues.length; j++) {
-//         if (typeof lineValues[j] === 'object') {
-//           console.log('linevalue[j]: ', lineValues[j]);
-//           console.log('typeof: ', typeof lineValues[j])
-//           lineValues[j] = lineValues[j].name;
-//         }
+//     let numberOfProducts = 0;
+//     let line = '';
+//     let productInfo = '';
+//     let createdDate, startDate;
+//     let id,
+//       orderDate, 
+//       orderStatus, 
+//       consumerName, 
+//       consumerComment, 
+//       producerComment, 
+//       totalOrderValue, 
+//       scheduleType, 
+//       scheduleDate, 
+//       city, 
+//       province, 
+//       address, 
+//       fee, 
+//       feeWaiver,
+//       productName,
+//       productQty,
+//       productTotalCost;
+//     // loop through each order in the array
+//     for (let i = 0; i < objArray.length; i++) {
+//       console.log('objectArr: ', objArray[i]); // **********************************************
+//       // break out the basic object information
+//       id = objArray[i].id;
+//       createdDate = new Date(objArray[i].orderDetails.createdDate);
+//       startDate = new Date(objArray[i].chosenSchedule.startDateTime);
+//       orderDate = createdDate.getMonth() + 1 + '/' +  createdDate.getDate() + '/' +  createdDate.getFullYear();
+//       orderStatus = objArray[i].orderDetails.orderStatus;
+//       consumerName = objArray[i].consumer.firstName;
+//       consumerComment = objArray[i].orderDetails.consumerComment;
+//       producerComment = objArray[i].orderDetails.producerComment;
+//       totalOrderValue = objArray[i].orderDetails.orderValue;
+//       scheduleType = objArray[i].chosenSchedule.type;
+//       scheduleDate = startDate.getMonth() + 1 + '/' +  startDate.getDate() + '/' + startDate.getFullYear();
+//       city = objArray[i].chosenSchedule.city;
+//       province = objArray[i].chosenSchedule.province;
+//       address = objArray[i].chosenSchedule.address;
+//       fee = objArray[i].chosenSchedule.fee;
+//       feeWaiver = objArray[i].chosenSchedule.feeWaiver;
+//       // get the number of products in the order
+//       numberOfProducts = objArray[i].orderDetails.productQuantities.length;
+//       // create a loop against the number of products
+//       for (let k = 0; k < numberOfProducts; k++) {
+//         // build the new line using the basic schedule info
+//         line = 
+//           id + ',' + 
+//           orderDate + ',' + 
+//           orderStatus + ',' + 
+//           consumerName + ',' + 
+//           consumerComment + ',' + 
+//           producerComment + ',' + 
+//           totalOrderValue + ',' + 
+//           scheduleType + ',' + 
+//           scheduleDate + ',' + 
+//           city + ',' + 
+//           province + ',' + 
+//           address + ',' + 
+//           fee + ',' + 
+//           feeWaiver + ',';
+//         // get the product info
+//         productQty = objArray[i].orderDetails.productQuantities[k].orderQuantity;
+//         productTotalCost = objArray[i].orderDetails.productQuantities[k].orderValue;
+//         productName = this.returnValueFromArrayByAttributeValue(objArray[i].productList, 'name', 'id', objArray[i].orderDetails.productQuantities[k].productId); 
+//         productInfo = productName + ',' + productQty + ',' + productTotalCost;
+//         // add the product info to the new line
+//         line += productInfo;
+//         // replace any carriage returns
+//         line = line.replace(/\r\n|\r|\n/gm, ' ');
+//         // force line to string
+//         line = line.toString();
+//         // append the new line to the returnString
+//         returnString += line + '\r\n';
+//         console.log('line this iteration: ', line);
 //       }
-//       console.log('linevalues: ', lineValues);
-//       let commaJoinedValues = lineValues.join(",");
-//       commaJoinedValues = commaJoinedValues.replace(/\r\n|\r|\n/gm, ' '); // replace carriage returns with spaces 
-//       console.log('cjv: ', commaJoinedValues);
-//       str += commaJoinedValues + '\r\n';
+      
+      
+//       // let lineValues = Object.keys(objArray[i]).map(key => objArray[i][key]);
+
+//       // for (let j = 0; j < lineValues.length; j++) {
+//       //   if (typeof lineValues[j] === 'object') {
+//       //     console.log('linevalue[j]: ', lineValues[j]);
+//       //     console.log('typeof: ', typeof lineValues[j])
+//       //     lineValues[j] = lineValues[j].name;
+//       //   }
+//       // }
+//       // console.log('linevalues: ', lineValues);
+//       // let commaJoinedValues = lineValues.join(",");
+//       // commaJoinedValues = commaJoinedValues.replace(/\r\n|\r|\n/gm, ' '); // replace carriage returns with spaces 
+//       // console.log('cjv: ', commaJoinedValues);
+      
 //     }
-//     console.log('string: ', str);
-//     return str;
+//     // console.log('array: ', array);
+//     // let values;
+//     // for (let i = 0; i < objArray.length; i++) {
+//     //   let line = '';
+//     //   console.log('objectArr: ', objArray[i]); // **********************************************
+//     //   let lineValues = Object.keys(objArray[i]).map(key => objArray[i][key]);
+
+//     //   for (let j = 0; j < lineValues.length; j++) {
+//     //     if (typeof lineValues[j] === 'object') {
+//     //       console.log('linevalue[j]: ', lineValues[j]);
+//     //       console.log('typeof: ', typeof lineValues[j])
+//     //       lineValues[j] = lineValues[j].name;
+//     //     }
+//     //   }
+//     //   console.log('linevalues: ', lineValues);
+//     //   let commaJoinedValues = lineValues.join(",");
+//     //   commaJoinedValues = commaJoinedValues.replace(/\r\n|\r|\n/gm, ' '); // replace carriage returns with spaces 
+//     //   console.log('cjv: ', commaJoinedValues);
+//     //   returnString += commaJoinedValues + '\r\n';
+//     // }
+//     console.log('string: ', returnString);
+//     return returnString;
 //   };
 
 //   convertAndDownload(yourData, recordType) {
@@ -166,6 +264,18 @@
 //     }
 //   };
 
+//   returnValueFromArrayByAttributeValue(array, returnAttibute, lookupAttribute, lookupValue) {
+//     let returnValue;
+//     for (let i = 0; i < array.length; i++) {
+//       if (array[i][lookupAttribute] === lookupValue) {
+//         console.log('lookup attribute: ', array[i][lookupAttribute]);
+//         console.log('lookup value: ', lookupValue)
+//         returnValue = array[i][returnAttibute];
+//       }
+//     }
+//     return returnValue;
+//   }
+
 //   capitalizeFirstLetter(string) {
 //     return string.charAt(0).toUpperCase() + string.slice(1);
 //   };
@@ -188,80 +298,159 @@ export class UtilityService {
 
   constructor() { }
 
-  ConvertProductToCSV(objArray) {
-    let array = typeof objArray !== 'object' ? JSON.parse(objArray) : objArray;
-    let str = '';
-    let row = '';
-
-    let keys = Object.keys(objArray[0]);
-      keys.forEach(name => {
-        row += name + ',';
-      })
-
-    row = row.slice(0, -1);
-    console.log('row: ', row);
-    // append Label row with line break
-    str += row + '\r\n';
-    console.log('array: ', array);
-    let values;
-    for (let i = 0; i < objArray.length; i++) {
-      let line = '';
-      console.log('objectArr: ', objArray[i]);
-      let lineValues = Object.keys(objArray[i]).map(key => objArray[i][key]);
-
-      for (let j = 0; j < lineValues.length; j++) {
-        if (typeof lineValues[j] === 'object') {
-          console.log('linevalue[j]: ', lineValues[j]);
-          console.log('typeof: ', typeof lineValues[j])
-          lineValues[j] = lineValues[j].name;
-        }
-      }
-      console.log('linevalues: ', lineValues);
-      let commaJoinedValues = lineValues.join(",");
-      commaJoinedValues = commaJoinedValues.replace(/\r\n|\r|\n/gm, ' '); // replace carriage returns with spaces 
-      console.log('cjv: ', commaJoinedValues);
-      str += commaJoinedValues + '\r\n';
-    }
-    console.log('string: ', str);
-    return str;
-  };
-
   ConvertScheduleToCSV(objArray) {
     let array = typeof objArray !== 'object' ? JSON.parse(objArray) : objArray;
-    let str = '';
+    let returnString = '';
     let row = '';
 
-    let keys = Object.keys(objArray[0]);
-      keys.forEach(name => {
-        row += name + ',';
-      })
-
-    row = row.slice(0, -1);
-    console.log('row: ', row);
+    // create the header row, append to returnString
+    let headers = 'ID,Type,Address,City,Province,Description,Date,Start Time,End Time,Order Deadline Date,Order Deadline Time,Fee,Fee Waiver,Number of Orders';
     // append Label row with line break
-    str += row + '\r\n';
-    console.log('array: ', array);
-    let values;
+    returnString += headers + '\r\n';
+    let line = '';
+    let startDateTime,
+      endDateTime,
+      orderDeadlineDateTime;
+    let id,
+      type,
+      address,
+      city,
+      province,
+      description,
+      date,
+      startTime,
+      endTime,
+      orderDeadlineDate,
+      orderDeadlineTime,
+      fee,
+      feeWaiver,
+      orderCount;
+    
+    // loop through each order in the array
     for (let i = 0; i < objArray.length; i++) {
-      let line = '';
-      console.log('objectArr: ', objArray[i]);
-      let lineValues = Object.keys(objArray[i]).map(key => objArray[i][key]);
+      console.log('objectArr: ', objArray[i]); // **********************************************
+      startDateTime = new Date(objArray[i].startDateTime);
+      endDateTime = new Date(objArray[i].endDateTime);
+      orderDeadlineDateTime = new Date(objArray[i].orderDeadline);
+      id = objArray[i].id;
+      type = objArray[i].type;
+      address = objArray[i].address;
+      city = objArray[i].city;
+      province = objArray[i].province;
+      description = objArray[i].description;
+      date = startDateTime.getMonth() + 1 + '/' +  startDateTime.getDate() + '/' +  startDateTime.getFullYear();
+      startTime = startDateTime.getHours() + ':' +  startDateTime.getMinutes();
+      endTime = endDateTime.getHours() + ':' +  endDateTime.getMinutes();
+      orderDeadlineDate = orderDeadlineDateTime.getMonth() + 1 + '/' +  orderDeadlineDateTime.getDate() + '/' +  orderDeadlineDateTime.getFullYear();
+      orderDeadlineTime = orderDeadlineDateTime.getHours() + ':' +  orderDeadlineDateTime.getMinutes();
+      fee = objArray[i].fee;
+      feeWaiver = objArray[i].feeWaiver;
+      orderCount = objArray[i].orderCount
 
-      for (let j = 0; j < lineValues.length; j++) {
-        if (typeof lineValues[j] === 'object') {
-          console.log('linevalue[j]: ', lineValues[j]);
-          console.log('typeof: ', typeof lineValues[j])
-          lineValues[j] = lineValues[j].name;
-        }
-      }
-      console.log('linevalues: ', lineValues);
-      let commaJoinedValues = lineValues.join(",");
-      commaJoinedValues = commaJoinedValues.replace(/\r\n|\r|\n/gm, ' '); // replace carriage returns with spaces 
-      console.log('cjv: ', commaJoinedValues);
-      str += commaJoinedValues + '\r\n';
-    }
-    console.log('string: ', str);
-    return str;
+      // create the line
+      line = 
+        id + ',' + 
+        type + ',' +
+        address + ',' +
+        city + ',' +
+        province + ',' +
+        description + ',' + 
+        date + ',' +
+        startTime + ',' +
+        endTime + ',' +
+        orderDeadlineDate + ',' +
+        orderDeadlineTime + ',' +
+        fee + ',' +
+        feeWaiver + ',' +
+        orderCount;
+
+      // replace any carriage returns
+      line = line.replace(/\r\n|\r|\n/gm, ' ');
+      // force line to string
+      line = line.toString();
+      // append the new line to the returnString
+      returnString += line + '\r\n';
+      console.log('line this iteration: ', line);
+    };
+    // return the entire string
+    console.log('string: ', returnString);
+    return returnString;
+  };
+
+  ConvertProductToCSV(objArray) {
+    let array = typeof objArray !== 'object' ? JSON.parse(objArray) : objArray;
+    let returnString = '';
+    let row = '';
+
+    // create the header row, append to returnString
+    let headers = 'ID,Name,Description,Image Path,Price Per Unit,Unit,Units Per Final Product,Category,Subcategory,Date Added,Qty Available,Qty Pending,Qty Accepted,Qty Completed,Obsolete?';
+    // append Label row with line break
+    returnString += headers + '\r\n';
+    let line = '';
+    let createdDate;
+    let id,
+      name,
+      description,
+      imagePath,
+      pricePerUnit,
+      unit,
+      unitsPerFinal,
+      category,
+      subcategory,
+      dateAdded,
+      qtyAvailable,
+      qtyPending,
+      qtyAccepted,
+      qtyCompleted,
+      isObsolete;
+    
+    // loop through each order in the array
+    for (let i = 0; i < objArray.length; i++) {
+      console.log('objectArr: ', objArray[i]); // **********************************************
+      id = objArray[i].id;
+      name = objArray[i].name;
+      description = objArray[i].description;
+      imagePath = objArray[i].image;
+      pricePerUnit = objArray[i].pricePerUnit;
+      unit = objArray[i].unit;
+      unitsPerFinal = objArray[i].unitsPer;
+      category = objArray[i].category;
+      subcategory = objArray[i].subcategory;
+      createdDate = new Date(objArray[i].dateAdded);
+      dateAdded = createdDate.getMonth() + 1 + '/' +  createdDate.getDate() + '/' +  createdDate.getFullYear();
+      qtyAvailable = objArray[i].qtyAvailable;
+      qtyPending = objArray[i].qtyPending;
+      qtyAccepted = objArray[i].qtyAccepted;
+      qtyCompleted = objArray[i].qtyCompleted;
+      isObsolete = objArray[i].isObsolete;
+      // create the line
+      line = 
+        id + ',' + 
+        name + ',' +
+        description + ',' + 
+        imagePath + ',' + 
+        pricePerUnit + ',' + 
+        unit + ',' + 
+        unitsPerFinal + ',' + 
+        category + ',' + 
+        subcategory + ',' + 
+        dateAdded + ',' + 
+        qtyAvailable + ',' + 
+        qtyPending + ',' + 
+        qtyAccepted + ',' + 
+        qtyCompleted + ',' + 
+        isObsolete;
+      // replace any carriage returns
+      line = line.replace(/\r\n|\r|\n/gm, ' ');
+      // force line to string
+      line = line.toString();
+      // append the new line to the returnString
+      returnString += line + '\r\n';
+      console.log('line this iteration: ', line);
+    };
+    // return the entire string
+    console.log('string: ', returnString);
+    return returnString;
   };
 
   ConvertOrderToCSV(objArray) {
@@ -272,15 +461,7 @@ export class UtilityService {
     // create the header row, append to returnString
     let headers = 'ID,Order Date,Order Status,Consumer Name,Consumer Comment,Producer Comment,Total Order Value,Schedule Type,Schedule Date,City,Province,Address,Fee,Fee Waiver,Product Name,Quantity,Product Total Cost';
 
-    // let keys = Object.keys(objArray[0]);
-    //   keys.forEach(name => {
-    //     row += name + ',';
-    //   })
-
-    // row = row.slice(0, -1);
-    // console.log('row: ', row);  // **********************************************
     // append Label row with line break
-    // returnString += row + '\r\n';
     returnString += headers + '\r\n';
 
     let numberOfProducts = 0;
@@ -403,14 +584,17 @@ export class UtilityService {
   convertAndDownload(yourData, recordType) {
     console.log('recordtype: ', recordType);
     console.log('data received: ', yourData);
-    let csvData;
+    let csvData, fileName;
     if (recordType === 'schedule') {
+      fileName = 'schedules.csv';
       csvData = this.ConvertScheduleToCSV(yourData);
     }
     if (recordType === 'product') {
+      fileName = 'products.csv';
       csvData = this.ConvertProductToCSV(yourData);
     }
     if (recordType === 'order') {
+      fileName = 'orders.csv';
       csvData = this.ConvertOrderToCSV(yourData);
     }
     
@@ -420,7 +604,7 @@ export class UtilityService {
     let blob = new Blob([csvData], { type: 'text/csv' });
     let url= window.URL.createObjectURL(blob);
     a.href = url;
-    a.download = 'Results.csv'; /* your file name*/
+    a.download = fileName; /* your file name*/
     a.click();
     return 'success';
   };
