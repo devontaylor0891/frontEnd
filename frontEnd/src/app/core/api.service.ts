50056867c179517f7be51e1e39233b552b88b93c
Devon <devontaylor0891@gmail.com>
2019-06-21 13:27:48 -0600

    Reset api target.

--- frontEnd/src/app/core/api.service.ts
+++ frontEnd/src/app/core/api.service.ts
@@ -18,7 +18,7 @@ export class ApiService {
   // apiUrl = 'http://localhost:3000';
   // apiUrl = 'http://onlylocalfood-api.a3jw4x3uey.us-west-2.elasticbeanstalk.com/api';
   // apiUrl = 'http://api-server-env.u4xn8vrpir.us-west-2.elasticbeanstalk.com/api';
-  apiUrl = 'https://api.olfdev.com/api';
+  apiUrl = 'https://api.onlylocalfood.com/api';
 
   productsUrl = '../../../../assets/api/products/';
   producerUrl = '../../../../assets/api/producer/';
