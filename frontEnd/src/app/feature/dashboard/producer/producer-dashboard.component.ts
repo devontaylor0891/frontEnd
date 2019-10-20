a4672eaf2e820abf97b00e289a15a2ffc0a2e064
devontaylor0891 <devontaylor0891@gmail.com>
2019-10-19 20:02:20 -0600

    Add consumer phone to order modal. Fix active prod dash selector error.

--- frontEnd/src/app/feature/dashboard/producer/producer-dashboard.component.ts
+++ frontEnd/src/app/feature/dashboard/producer/producer-dashboard.component.ts
@@ -1,91 +1,3 @@
-// // import { Component, OnInit, Input, OnChanges } from '@angular/core';
-
-// // import { ProducerDashboardService } from '../producer-dashboard.service';
-
-// // import { UserModel } from '../../../core/models/user.model';
-// // import { ProducerModel } from '../../../core/models/producer.model';
-
-// // @Component({
-// //   selector: 'app-producer-dashboard',
-// //   templateUrl: './producer-dashboard.component.html',
-// //   styleUrls: ['./producer-dashboard.component.scss'],
-// //   providers: []
-// // })
-// // export class ProducerDashboardComponent implements OnInit, OnChanges {
-
-// //   @Input() id: number;
-// //   @Input() user: UserModel;
-// //   producer: ProducerModel;
-
-// //   ngOnChanges() {};
-
-// //   constructor(private dashboardService: ProducerDashboardService) { }
-
-// //   ngOnInit() {
-
-// //     this.dashboardService.getProducer()
-// //       .subscribe(
-// //         result => {
-// //           this.producer = result;
-// //         }
-// //       );
-
-// //     this.dashboardService.loadData(this.id);
-
-// //   }
-
-// // }
-
-
-// import { Component, OnInit, Input, OnChanges } from '@angular/core';
-
-// import { ProducerDashboardService } from '../producer-dashboard.service';
-// import { ApiService } from '../../../core/api.service';
-
-// import { UserModel } from '../../../core/models/user.model';
-// import { ProducerModel } from '../../../core/models/producer.model';
-
-// @Component({
-//   selector: 'app-producer-dashboard',
-//   templateUrl: './producer-dashboard.component.html',
-//   styleUrls: ['./producer-dashboard.component.scss'],
-//   providers: []
-// })
-// export class ProducerDashboardComponent implements OnInit, OnChanges {
-
-//   @Input() id: number;
-//   @Input() user: UserModel;
-//   producer: ProducerModel;
-//   customUrl: string;
-
-//   ngOnChanges() {};
-
-//   constructor(private dashboardService: ProducerDashboardService,
-//               private apiService: ApiService) { }
-
-//   ngOnInit() {
-
-//     this.dashboardService.getProducer()
-//       .subscribe(
-//         result => {
-//           this.producer = result;
-//           if (this.producer) {
-//             this.apiService.getCustomUrlByProducerId(this.producer.id)
-//               .subscribe(
-//                 result => {
-//                   this.customUrl = result[0].customUrl;
-//                 }
-//               )
-//           } 
-//         }
-//       );
-
-//     this.dashboardService.loadData(this.id);
-
-//   }
-
-// }
-
 import { Component, OnInit, Input, OnChanges } from '@angular/core';
 
 import { ProducerDashboardService } from '../producer-dashboard.service';
@@ -106,6 +18,7 @@ export class ProducerDashboardComponent implements OnInit, OnChanges {
   @Input() user: UserModel;
   producer: ProducerModel;
   customUrlObject: any;
+  activeLink = 'productsLink';
 
   ngOnChanges() {};
 
@@ -122,17 +35,22 @@ export class ProducerDashboardComponent implements OnInit, OnChanges {
             // console.log('producer id: ', this.producer.id);
             this.apiService.getCustomUrlByProducerId(this.producer.id)
               .subscribe(
-                result => {
+                results => {
                   // console.log('custom result: ', result);
-                  this.customUrlObject = result[0];
+                  this.customUrlObject = results[0];
                 }
               )
-          } 
+          }
         }
       );
 
     this.dashboardService.loadData(this.id);
 
   }
+
+  setActive(arg) {
+    this.activeLink = arg;
+  }
+
 
 }
